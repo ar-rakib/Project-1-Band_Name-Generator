c5c1ffa2a947ec7114b0f727b2f8d54ad965af79
ar-rakib <arrakib.github.com>
2023-11-30 07:16:46 +0600

    First Project Band Name generator

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator \n")
+
+city_name = input("What is the name of the city you grew up in?\n")
+
+pet_name = input("what is your pet name ?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
